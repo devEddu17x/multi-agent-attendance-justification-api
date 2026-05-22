669efd85a103371b1ca4a18ea38e94019b242e9d
eddu <azabacheeduardo85@gmail.com>
2026-05-22 11:29:03 -0500

    feat: add TeachersModule and ConfigModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './core/config/config.module';
+import { TeachersModule } from './modules/teachers/teachers.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, TeachersModule],
   controllers: [AppController],
   providers: [AppService],
 })
