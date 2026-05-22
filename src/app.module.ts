542df5a21b751df1c5a65df532eb3625e6881514
eddu <azabacheeduardo85@gmail.com>
2026-05-22 16:00:08 -0500

    feat: add ParentModule to AppModule imports for parent management

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './core/config/config.module';
 import { TeachersModule } from './modules/teachers/teachers.module';
+import { ParentModule } from './modules/parents/parent.module';
 
 @Module({
-  imports: [ConfigModule, TeachersModule],
+  imports: [ConfigModule, TeachersModule, ParentModule],
   controllers: [AppController],
   providers: [AppService],
 })
