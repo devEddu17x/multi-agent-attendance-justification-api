84ee57ef7713267acd48ecaf5c72c6fd342c3f2e
eddu <azabacheeduardo85@gmail.com>
2026-05-23 14:37:24 -0500

    feat: add TeacherCourseEntity to TeachersModule imports for course integration

--- src/modules/teachers/teachers.module.ts
+++ src/modules/teachers/teachers.module.ts
@@ -3,11 +3,14 @@ import { TeachersService } from './teachers.service';
 import { TeachersController } from './teachers.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TeacherEntity } from './entities/teacher.entity';
+import { TeacherCourseEntity } from './entities/teacher-course.entity';
 import { UserEntity } from '../user/entities/user.entity';
 import { UserService } from '../user/user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, TeacherEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity, TeacherEntity, TeacherCourseEntity]),
+  ],
   controllers: [TeachersController],
   providers: [UserService, TeachersService],
 })
