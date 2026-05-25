34359a550d680508ae86fe3f37025fd1e6b7bf88
eddu <azabacheeduardo85@gmail.com>
2026-05-24 20:11:30 -0500

    refactor: update AdminModule to use AdministrationService and AdministrationController

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AdminService } from './admin.service';
-import { AdminController } from './admin.controller';
+import { AdministrationService } from './admin.service';
+import { AdministrationController } from './admin.controller';
 import { UserModule } from '../user/user.module';
 import { TeachersModule } from '../teachers/teachers.module';
 import { AuthModule } from '../auth/auth.module';
+import { ParentModule } from '../parents/parent.module';
 
 @Module({
-  imports: [AuthModule, UserModule, TeachersModule],
-  providers: [AdminService],
-  controllers: [AdminController],
+  imports: [AuthModule, UserModule, TeachersModule, ParentModule],
+  providers: [AdministrationService],
+  controllers: [AdministrationController],
 })
 export class AdminModule {}
