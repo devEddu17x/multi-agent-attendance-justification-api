f38523e220cf16d43290a1cc77eea00c3469f415
eddu <azabacheeduardo85@gmail.com>
2026-05-24 20:10:56 -0500

    feat: export ParentService from ParentModule

--- src/modules/parents/parent.module.ts
+++ src/modules/parents/parent.module.ts
@@ -10,5 +10,6 @@ import { UserModule } from '../user/user.module';
   imports: [UserModule, TypeOrmModule.forFeature([UserEntity, ParentEntity])],
   controllers: [ParentController],
   providers: [ParentService],
+  exports: [ParentService],
 })
 export class ParentModule {}
