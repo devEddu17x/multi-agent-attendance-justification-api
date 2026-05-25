ebba5d4df1239f05fe85035b63beb468acd4ba2f
eddu <azabacheeduardo85@gmail.com>
2026-05-24 20:11:19 -0500

    refactor: rename AdminController to AdministrationController and update service import

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { AdminService } from './admin.service';
+import { AdministrationService } from './admin.service';
 import { CreateTeacherDTO } from 'src/common/dtos/create-teacher.dto';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { ROLES } from 'src/common/enums/roles.enum';
@@ -7,13 +7,19 @@ import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 
 @UseGuards(JwtAuthGuard, RolesGuard)
-@Controller('admin')
-export class AdminController {
-  constructor(private readonly adminService: AdminService) {}
+@Controller('administration')
+export class AdministrationController {
+  constructor(private readonly adminService: AdministrationService) {}
 
   @Roles(ROLES.ADMIN)
   @Post('teacher')
   async createTeacher(@Body() dto: CreateTeacherDTO) {
+    return this.adminService.createTeacher(dto);
+  }
+
+  @Roles(ROLES.TEACHER)
+  @Post('parent')
+  async createParent(@Body() dto: CreateTeacherDTO) {
     return this.adminService.createTeacher(dto);
   }
 }
