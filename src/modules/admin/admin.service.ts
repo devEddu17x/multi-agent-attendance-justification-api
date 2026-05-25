c17752ea085bebfbe40293637e4ad3587820c3dd
eddu <azabacheeduardo85@gmail.com>
2026-05-24 20:11:09 -0500

    feat: implement createParent method in AdministrationService

--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -10,14 +10,17 @@ import { CognitoService } from '../auth/services/cognito.service';
 import { maskEmail } from 'src/utils/mask-email.util';
 import { CognitoUserParams } from '../auth/interfaces/cognito-user.interface';
 import { UserService } from '../user/user.service';
+import { CreateParentDto } from '../../common/dtos/create-parent.dto';
+import { ParentService } from '../parents/parent.service';
 
 @Injectable()
-export class AdminService {
-  private readonly logger = new Logger(AdminService.name);
+export class AdministrationService {
+  private readonly logger = new Logger(AdministrationService.name);
   constructor(
-    private readonly teachersService: TeachersService,
     private readonly cognitoService: CognitoService,
     private readonly userService: UserService,
+    private readonly teachersService: TeachersService,
+    private readonly parentService: ParentService,
   ) {}
   async createTeacher(dto: CreateTeacherDTO) {
     try {
@@ -42,6 +45,33 @@ export class AdminService {
       );
       throw new InternalServerErrorException(
         'Unable to create teacher. Please try again later.',
+      );
+    }
+  }
+
+  async createParent(dto: CreateParentDto) {
+    try {
+      const params: CognitoUserParams = {
+        email: dto.user.email,
+        password: dto.password,
+        firstName: dto.user.firstName,
+        lastName: dto.user.lastName,
+      };
+      const cognitoUser = await this.cognitoService.signUpUser(params);
+      const appUser = await this.userService.createUser(dto.user);
+      const teacher = await this.parentService.create({
+        ...dto,
+        userId: appUser.id,
+      });
+      return { teacher, cognitoUser };
+    } catch (error) {
+      if (error instanceof ConflictException) throw error;
+      this.logger.error(
+        `Failed to create parent with email ${maskEmail(dto.user.email)}`,
+        error,
+      );
+      throw new InternalServerErrorException(
+        'Unable to create parent. Please try again later.',
       );
     }
   }
