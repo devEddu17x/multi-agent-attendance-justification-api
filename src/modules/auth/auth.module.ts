6fbc6a335cfcbac6bcb4637bf7309bd8116b2a79
eddu <azabacheeduardo85@gmail.com>
2026-05-25 18:41:50 -0500

    feat: add JwtStrategy to AuthModule providers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CognitoService } from './services/cognito.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  providers: [CognitoService],
+  providers: [CognitoService, JwtStrategy],
   exports: [CognitoService],
 })
 export class AuthModule {}
