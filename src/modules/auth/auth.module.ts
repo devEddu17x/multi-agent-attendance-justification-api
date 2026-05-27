202056259f6bf8dce8badb18c8519ba8ae249ed9
eddu <azabacheeduardo85@gmail.com>
2026-05-26 20:38:55 -0500

    feat: add PassportModule to AuthModule for JWT strategy support

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { CognitoService } from './services/cognito.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   providers: [CognitoService, JwtStrategy],
-  exports: [CognitoService],
+  exports: [CognitoService, PassportModule],
 })
 export class AuthModule {}
