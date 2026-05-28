6984a79987e5551064a3678dcc04d9bac6b567ad
eddu <azabacheeduardo85@gmail.com>
2026-05-28 11:51:08 -0500

    feat(tests): simplify teacher token generation by removing Cognito confirmation logic

--- test/helpers/teacher-token.helper.ts
+++ test/helpers/teacher-token.helper.ts
@@ -1,28 +1,15 @@
 import { INestApplication } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { AdministrationService } from 'src/modules/admin/admin.service';
 import { App } from 'supertest/types';
 import { authenticateUser } from './authenticate-user.helper';
 import { getRandomCreateTeacherDTO } from './../factories/teacher-random.dto';
-import {
-  CognitoIdentityProvider,
-  AdminConfirmSignUpCommand,
-} from '@aws-sdk/client-cognito-identity-provider';
+import { confirmUser } from './confirm-user.helper';
 
 export const getTeacherToken = async (app: INestApplication<App>) => {
   const dto = getRandomCreateTeacherDTO();
   const administrationService = app.get(AdministrationService);
   await administrationService.createTeacher(dto);
-
-  const { region, userPoolId } = app.get(ConfigService).get('cognito');
-  const cognitoClient = new CognitoIdentityProvider({ region });
-  await cognitoClient.send(
-    new AdminConfirmSignUpCommand({
-      UserPoolId: userPoolId,
-      Username: dto.user.email,
-    }),
-  );
-
+  await confirmUser(app, dto.user.email);
   const idToken = await authenticateUser(app, dto.user.email, dto.password);
   return idToken;
 };
