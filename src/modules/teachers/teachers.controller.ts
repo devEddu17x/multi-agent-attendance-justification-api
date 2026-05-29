db6cce1c055c271d845f04c4457d87f47a49bd38
eddu <azabacheeduardo85@gmail.com>
2026-05-28 20:29:49 -0500

    feat: add api doc for teachers controller

--- src/modules/teachers/teachers.controller.ts
+++ src/modules/teachers/teachers.controller.ts
@@ -9,17 +9,21 @@ import {
 } from '@nestjs/common';
 import { TeachersService } from './teachers.service';
 import { UpdateTeacherDTO } from './dto/update-teacher.dto';
+import { ApiDocGetAllTeachers } from './docs/teachers.doc';
+import { ApiDocGetTeacherById } from './docs/teachers.doc';
 
 @Controller('teachers')
 export class TeachersController {
   constructor(private readonly teachersService: TeachersService) {}
 
   @Get()
+  @ApiDocGetAllTeachers()
   getAll() {
     return this.teachersService.getAll();
   }
 
   @Get(':id')
+  @ApiDocGetTeacherById()
   getById(@Param('id', ParseUUIDPipe) id: string) {
     return this.teachersService.getById(id);
   }
