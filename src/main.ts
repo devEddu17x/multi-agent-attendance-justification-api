ea531714527128d3f5132b363afa53ec7e8b8295
eddu <azabacheeduardo85@gmail.com>
2026-05-28 22:04:52 -0500

    feat: add bearer auth in swagger doc builder

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,7 @@ async function bootstrap() {
       'API documentation for the Multi Agent Attendance Justification application',
     )
     .setVersion('1.0.0')
+    .addBearerAuth()
     .build();
 
   const documentFactory = () => {
