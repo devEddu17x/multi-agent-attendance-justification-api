c9ab1626b227a5316d0a747677196008532f590f
eddu <azabacheeduardo85@gmail.com>
2026-05-28 20:28:22 -0500

    fix: remove partial type

--- src/modules/classroom/dto/update-classroom.dto.ts
+++ src/modules/classroom/dto/update-classroom.dto.ts
@@ -1,4 +1,16 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateClassroomDTO } from './create-classroom.dto';
+import { IsInt, Min, Max, IsString, IsNotEmpty, IsUUID } from 'class-validator';
 
-export class UpdateClassroomDto extends PartialType(CreateClassroomDTO) {}
+export class UpdateClassroomDto {
+  @IsInt()
+  @Min(1)
+  @Max(64)
+  capacity: number;
+
+  @IsString()
+  @IsNotEmpty()
+  building: string;
+
+  @IsNotEmpty()
+  @IsUUID()
+  tutorId?: string;
+}
