7dd718370afb9cb3b97af26cf6b59edf40e14f3b
eddu <azabacheeduardo85@gmail.com>
2026-05-28 22:04:24 -0500

    feat: add auth controller

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { CognitoService } from './services/cognito.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+  controllers: [AuthController],
   providers: [CognitoService, JwtStrategy],
   exports: [CognitoService, PassportModule],
 })
