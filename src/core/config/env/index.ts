5e80296168fc09a37b0aa15bbe55d4c845385a22
eddu <azabacheeduardo85@gmail.com>
2026-05-28 19:06:24 -0500

    feat(rekognition): add AWS Rekognition configuration and environment variable validation

--- src/core/config/env/index.ts
+++ src/core/config/env/index.ts
@@ -1,3 +1,4 @@
 export { default as typeormConfig } from './typeorm.config';
 export { default as cognitoConfig } from './cognito.config';
 export { default as apiConfig } from './api.config';
+export { default as rekognitionConfig } from './rekognition.config';
