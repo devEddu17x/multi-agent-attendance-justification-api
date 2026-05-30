317dfce709baa6a6f1ed9d0fafb4f59b207e1a74
eddu <azabacheeduardo85@gmail.com>
2026-05-30 18:49:21 -0500

    fix: correct class name casing for CreateScheduleDTO

--- src/modules/academic-classes/dto/create-schedule.dto.ts
+++ src/modules/academic-classes/dto/create-schedule.dto.ts
@@ -1,7 +1,7 @@
 import { IsEnum, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { DayOfWeek } from '../../../common/enums/day-of-week.enum';
 
-export class CreateScheduleDto {
+export class CreateScheduleDTO {
   @IsUUID()
   @IsNotEmpty()
   teacherId: string;
