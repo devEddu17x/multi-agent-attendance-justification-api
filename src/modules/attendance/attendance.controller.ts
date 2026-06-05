6d07d67a9c0235c11b110f2db73c4c1bb7fb5c6c
eddu <azabacheeduardo85@gmail.com>
2026-06-05 09:56:57 -0500

    feat: enhance attendance controller with role-based access and date parsing for schedule queries

--- src/modules/attendance/attendance.controller.ts
+++ src/modules/attendance/attendance.controller.ts
@@ -7,6 +7,9 @@ import {
   UploadedFile,
   ParseUUIDPipe,
   Query,
+  Get,
+  UseGuards,
+  ParseDatePipe,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AttendanceService } from './services/attendance.service';
@@ -15,11 +18,19 @@ import {
   ApiDocRegisterStudentFace,
   ApiDocRegisterAttendance,
 } from './docs/attendance.doc';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { ROLES } from 'src/common/enums/roles.enum';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import type { User } from 'src/common/interfaces/user.interface';
 
 @Controller('attendance')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class AttendanceController {
   constructor(private readonly attendanceService: AttendanceService) {}
 
+  @Roles(ROLES.ADMIN, ROLES.TEACHER)
   @Put(':id/rekognition')
   @UseInterceptors(FileInterceptor('photo'))
   @ApiDocRegisterStudentFace()
@@ -30,12 +41,13 @@ export class AttendanceController {
     return await this.attendanceService.registerStudentFace(id, photo);
   }
 
+  @Roles(ROLES.ADMIN, ROLES.TEACHER)
   @Post('schedule')
   @UseInterceptors(FileInterceptor('photo'))
   @ApiDocRegisterAttendance()
   async registerAttendance(
     @UploadedFile(ImageValidationPipe(50)) photo: Express.Multer.File,
-    @Query('time') time?: string,
+    @Query('time', new ParseDatePipe({})) time?: Date,
   ) {
     console.log('time', time);
     const simulatedTime = time ? new Date(time) : undefined;
@@ -43,6 +55,38 @@ export class AttendanceController {
     return await this.attendanceService.registerAttendance(
       photo,
       simulatedTime,
+    );
+  }
+
+  // @Roles(ROLES.PARENT)
+  // @Get('parent')
+  // async getAttendanceFromParentStudents(@CurrentUser() user: User) {
+  //   return await this.attendanceService.getParentStudentAttendance(user.email);
+  // }
+
+  @Roles(ROLES.PARENT)
+  @Get('parent/student/:id')
+  async getAttendanceFromStudent(
+    @CurrentUser() user: User,
+    @Param('id', ParseUUIDPipe) id: string,
+  ) {
+    return await this.attendanceService.getParentStudentAttendanceByStudentId(
+      user.sub,
+      id,
+    );
+  }
+
+  @Roles(ROLES.TEACHER)
+  @Get('teacher/schedule/:id')
+  async getAttendanceFromSchedule(
+    @CurrentUser() user: User,
+    @Param('id', ParseUUIDPipe) id: string,
+    @Query('time', new ParseDatePipe({ optional: true })) time?: Date,
+  ) {
+    return await this.attendanceService.getTeacherStudentsAttendanceBySchedule(
+      user.sub,
+      id,
+      time,
     );
   }
 }
