1b74f6fe2bec4e261099df30e9804693c5230a5c
eddu <azabacheeduardo85@gmail.com>
2026-06-05 09:57:27 -0500

    feat: add missing entities to TypeOrmModule for enhanced attendance management

--- src/modules/attendance/attendance.module.ts
+++ src/modules/attendance/attendance.module.ts
@@ -5,6 +5,10 @@ import { AttendanceService } from './services/attendance.service';
 import { RekognitionService } from './services/rekognition.service';
 import { AttendanceEntity } from './entities/attendance.entity';
 import { AttendanceJustificationEntity } from './entities/attendance-justification.entity';
+import { ParentEntity } from '../parents/entities/parent.entity';
+import { TeacherEntity } from '../teachers/entities/teacher.entity';
+import { ScheduleEntity } from '../academic-classes/entities/schedule.entity';
+import { EnrollmentEntity } from '../academic-classes/entities/enrollment.entity';
 import { StudentsModule } from '../students/students.module';
 import { AcademicClassesModule } from '../academic-classes/academic-classes.module';
 
@@ -12,7 +16,14 @@ import { AcademicClassesModule } from '../academic-classes/academic-classes.modu
   imports: [
     StudentsModule,
     AcademicClassesModule,
-    TypeOrmModule.forFeature([AttendanceEntity, AttendanceJustificationEntity]),
+    TypeOrmModule.forFeature([
+      AttendanceEntity,
+      AttendanceJustificationEntity,
+      ParentEntity,
+      TeacherEntity,
+      ScheduleEntity,
+      EnrollmentEntity,
+    ]),
   ],
   controllers: [AttendanceController],
   providers: [AttendanceService, RekognitionService],
