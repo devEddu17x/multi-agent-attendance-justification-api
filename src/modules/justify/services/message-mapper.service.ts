5fa8943749fc7bf9a1d1399484ec4def277af98e
eddu <azabacheeduardo85@gmail.com>
2026-06-08 19:58:03 -0500

    refactor: remove unused appendMultimodal method and related import

--- src/modules/justify/services/message-mapper.service.ts
+++ src/modules/justify/services/message-mapper.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { HumanMessage, AIMessage, BaseMessage } from '@langchain/core/messages';
 import { ChatMessageEntity } from '../entities/chat-message.entity';
 import { ChatMessageRole } from '../enums/chat-message-role.enum';
-import { AnthropicContentBlock } from '../../../agents/interfaces/agent-state.interface';
 
 @Injectable()
 export class MessageMapperService {
@@ -12,26 +11,6 @@ export class MessageMapperService {
         return new HumanMessage(m.content);
       }
       return new AIMessage(m.content);
-    });
-  }
-
-  appendMultimodal(
-    messages: BaseMessage[],
-    content: string,
-    blocks: AnthropicContentBlock[],
-  ): void {
-    const lastMessage = messages[messages.length - 1];
-    if (!(lastMessage instanceof HumanMessage)) {
-      return;
-    }
-
-    const contentBlocks: any[] = [{ type: 'text', text: content }];
-    for (const block of blocks) {
-      contentBlocks.push(block);
-    }
-
-    messages[messages.length - 1] = new HumanMessage({
-      content: contentBlocks,
     });
   }
 }
